3b214563e42612cda184b3b1ac6bec08df593b57
Alison Michan <alisonmichan@Alisons-MacBook-Pro.local>
2022-05-13 14:54:46 +0200

    initial commit

---
+++ read_data/connect_and_read.py
@@ -0,0 +1,87 @@
+#connect to discord using authentication token and read chat data
+
+import websocket #pip install websocet-client
+import json
+import threading
+import time
+import os
+from datetime import datetime
+import requests
+import datetime
+from dotenv import load_dotenv
+
+load_dotenv()
+
+def send_json_request(ws, request):
+    ws.send(json.dumps(request))
+
+
+def receive_json_response(ws):
+    response = ws.recv()
+    if response:
+        return (json.loads(response))
+
+
+def heartbeat(interval, ws):
+    print("heartbeat begins")
+    while True:
+        time.sleep(interval)
+        heartbeatJSON = {
+            "op":1,
+            "d": "null"
+        }
+        send_json_request(ws, heartbeatJSON)
+        print("heartbeat sent")
+
+
+
+if __name__=='__main__':
+    print('starting local read discord server : ', os.getenv('SERVER_NAME'))
+
+    ws = websocket.WebSocket()
+    ws.connect('wss://gateway.discord.gg/?v=8&encording=json')
+    event = receive_json_response(ws)
+    heartbeat_interval = event['d']['heartbeat_interval'] /1000
+    threading._start_new_thread(heartbeat, (heartbeat_interval, ws))
+
+
+    token = os.getenv('TOKEN')
+
+    payload = {
+      "op": 2,
+      "d": {
+        "token": token,
+        "intents": 513,
+        "properties": {
+          "$os": "osx",
+          "$browser": "chrome",
+          "$device": "pc"
+        }
+      }
+    }
+
+    send_json_request(ws, payload)
+
+    while True:
+        event = receive_json_response(ws)
+
+        try:
+            guild_id = event['d']['guild_id']
+            channel_id = event['d']['channel_id']
+            timestamp = event['d']['timestamp']
+            username = event['d']['author']['username']
+            content = event['d']['content']
+
+            #post request to the flask app
+            print(guild_id, channel_id, timestamp, username, content)
+            record = {'guild_id':int(guild_id), 'channel_id':int(channel_id), 'time':str(timestamp)}
+            r = requests.post(os.getenv(LOG_URL), json=record)
+            print('server response: ', r)
+
+            op_code = event('op')
+            if op_code == 11:
+                print('heartbeat received')
+
+        except:
+            print('exception')
+            pass
---
+++ view_data/receive_and_write.py
@@ -0,0 +1,63 @@
+from flask import Flask, request
+import os
+import sqlite3
+import json
+from datetime import datetime
+import psycopg2
+from dotenv import load_dotenv
+load_dotenv()
+
+cur_dir = os.path.dirname(__file__)
+db = os.path.join(cur_dir, 'discord_log.sqlite')
+
+
+
+def pg_entry(event):
+    try:
+        conn = psycopg2.connect(user = os.getenv('USER'),
+                                password = os.getenv('PASSWORD'),
+                                host = os.getenv('LOCALHOST'),
+                                port = os.getenv('PORT'),
+                                database = os.getenv('DB')
+                                )
+
+        cursor = conn.cursor()
+
+        # list that contain records to be inserted into table
+        data = [event.get("guild_id"), event.get("channel_id"), event.get("time")]
+        print(data)
+
+        #insert data
+        cursor.execute("INSERT into teststats(guild_id, channel_id, time) VALUES (%s, %s, %s)", data)
+
+        print("inserted successfully...")
+
+        # Commit your changes in the database
+        conn.commit()
+
+        # Closing the connection
+        conn.close()
+
+    except (Exception, psycopg2.Error) as error :
+        print ("Error while connecting to PostgreSQL", error)
+
+
+
+app = Flask(__name__)
+
+@app.route('/', methods=['POST'])
+def index():
+    print('receiving server')
+    incoming_event = request.get_json()
+
+    data = request.data
+    data = json.loads(data)
+
+    print(incoming_event)
+    #print(**incoming_event)
+    #sqlite_entry(db, incoming_event)
+    pg_entry(incoming_event)
+    return {'message': 'success'}
+
+if __name__ == '__main__':
+    app.run(host="localhost", port=5000, debug=True)
